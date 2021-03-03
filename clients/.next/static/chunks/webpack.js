1f9e07c618d47155fb1d98c6b8d045f49eae62d7
SakibHasanCse <sakibhasan9641@gmail.com>
2021-03-03 10:55:17 +0600

    setup update blog

--- clients/.next/static/chunks/webpack.js
+++ clients/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "556829d4dda4a086a0ae";
+/******/ 	var hotCurrentHash = "f565a9d842f05f16c167";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ clients/components/crud/update-blog.js
@@ -0,0 +1,7 @@
+
+const UpdateBlog = () => {
+    return (
+        <div>Update Blog</div>
+    )
+}
+export default UpdateBlog
