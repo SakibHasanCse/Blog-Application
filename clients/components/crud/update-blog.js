126a4611096eca9e5c61e14d570b89d65551c171
SakibHasanCse <sakibhasan9641@gmail.com>
2021-03-04 23:32:43 +0600

    update blog

--- clients/components/crud/update-blog.js
+++ clients/components/crud/update-blog.js
@@ -1,3 +1,17 @@
+import React, { useState, useEffect } from 'react'
+import { CreateBlog } from '../../actions/blog.js'
+import { isAuth, getCookie } from '../../actions/auth'
+import Router from 'next/router'
+import dynamic from 'next/dynamic'
+import { withRouter } from 'next/router'
+import { CategoryList } from '../../actions/category'
+import { TagsList } from '../../actions/tags'
+import Link from 'next/link'
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
+import 'react-quill/dist/quill.snow.css';
+import { quillformats, Quillmodules } from '../../helpers/quill.js'
+
+
 
 const UpdateBlog = () => {
     return (
